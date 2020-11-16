46d4e5a4e3921f55388d1446daf3062ae5add9b1
Alfredo <alfaradayepez@gmail.com>
2020-11-16 00:04:37 -0500

    Instalación y configuración de PHPUnit

---
+++ tests/HtmlElementTest.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace Test;
+
+use Tests\TestCase;
+
+class HtmlElementTest extends TestCase
+{
+    /** @test */
+    public function it_generates_html_elements()
+    {
+        $this->markTestIncomplete();
+    }
+
+}
