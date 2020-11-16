735697561c47320ad7be8fc0acc306c9c8022a59
Alfredo <alfaradayepez@gmail.com>
2020-11-15 23:19:03 -0500

    Creando ejemplo para refactorizar con  programación estructurada

--- app/HtmlElement.php
+++ app/HtmlElement.php
@@ -1,11 +1,66 @@
-<?php 
+<?php
 
 namespace App;
 
 class HtmlElement
 {
+    private $name;
+    private $content;
+    private $attributes;
+
+    public function __construct(string $name, array $attributes = [], $content = null)
+    {
+        $this->name = $name;
+        $this->attributes = $attributes;
+        $this->content = $content;
+    }
+
     public function render()
     {
-        return 'HTML ELEMENT HERE !';
+        // Si el elemento tiene atributos
+
+        if (!empty($this->attributes)) {
+
+            $htmlAttributes = '';
+
+            foreach ($this->attributes as $attribute => $value) {
+                
+                if (is_numeric($attribute)) {
+                    
+                    $htmlAttributes .= ' '.$value;
+                
+                } else {
+                    
+                    $htmlAttributes .= ' ' . $attribute . '="' . htmlentities($value, ENT_QUOTES, 'UTF-8') . '"';
+                }
+
+            }
+            // Abrir etiqueta con atributo
+
+            $result = '<' . $this->name . $htmlAttributes . '>';
+        } else {
+
+
+            //Abrir etiqueta sin atributos
+
+            $result = '<' . $this->name . '>';
+        }
+
+        // Si el elemento es Void
+
+        if (in_array($this->name, ['br', 'hr', 'img', 'img', 'input'])) {
+            // Retornar el resultado
+            return $result;
+        }
+
+        // Imprimir el contenido
+
+        $result .= htmlentities($this->content, ENT_QUOTES, 'UTF-8');
+
+        // Cerrar la etiqueta
+
+        $result .= '</' . $this->name . '>';
+
+        return $result;
     }
 }
--- public/index.php
+++ public/index.php
@@ -4,6 +4,38 @@ require '../vendor/autoload.php';
 
 use App\HtmlElement;
 
-$element = new HtmlElement;
+$element = new HtmlElement('p', [], 'Este es el contenido de la etiqueta');
 
-echo $element->render();
+echo htmlentities($element->render(), ENT_QUOTES, 'UTF-8');
+
+echo '<br><br>';
+
+$element = new HtmlElement('p', ['id' => 'my_paragraph'], 'Este es el contenido de la etiqueta');
+
+echo htmlentities($element->render(), ENT_QUOTES, 'UTF-8');
+
+echo '<br><br>';
+
+$element = new HtmlElement('p', ['id' => 'my_paragraph', 'class' => 'paragraph'], 'Este es el contenido de la etiqueta');
+
+echo htmlentities($element->render(), ENT_QUOTES, 'UTF-8');
+
+echo '<br><br>';
+
+$element = new HtmlElement('img', ['src' => 'img/styde.png']);
+
+echo htmlentities($element->render(), ENT_QUOTES, 'UTF-8');
+
+echo '<br><br>';
+
+$element = new HtmlElement('input', ['required']);
+
+echo htmlentities($element->render(), ENT_QUOTES, 'UTF-8');
+
+echo '<br><br>';
+
+$element = new HtmlElement('img', ['src' => 'img/styde.png', 'title' => 'Curso de "Refactorización" en styde']);
+
+echo htmlentities($element->render(), ENT_QUOTES, 'UTF-8');
+
+echo '<br><br>';
